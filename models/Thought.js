0a11ade201ed72a5155b77c459ffaa99bbad2f5c
Callank21 <95372216+Callank21@users.noreply.github.com>
2022-06-19 14:40:48 -0700

    finishing set up

---
+++ models/Thought.js
@@ -0,0 +1,54 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+)
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+        type: String,
+        required: true,
+        min: [1, 'Number between 1 & 280'],
+        max: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
